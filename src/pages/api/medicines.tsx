69530f73ad164df3387a8d1b085c8afc5495aec5
Arthur <arthurmlamas@gmail.com>
2022-04-25 16:56:41 -0300

    fixed new medicine conflict registration

--- src/pages/api/medicines.tsx
+++ src/pages/api/medicines.tsx
@@ -2,56 +2,57 @@ import { NextApiRequest, NextApiResponse } from "next";
 import connect from '../../../utils/database'
 
 interface ErrorResponseType{
-    error: string
+  error: string
 }
 
 interface SucessResponseType{
-    _id: string;
-    name: string;
+  _id: string;
+  name: string;
 }
 
 export default async(
-    req: NextApiRequest,
-    res: NextApiResponse<ErrorResponseType | SucessResponseType>
+  req: NextApiRequest,
+  res: NextApiResponse<ErrorResponseType | SucessResponseType>
 ): Promise<void> => {
-    
 
-    if (req.method === "POST"){
+  const {db} = await connect();
 
-        const{ name, quantity } = req.body;
+  const medicineNameExists = await db
+  .collection('medicines')
+  .findOne({ name: name})
 
-        if( !name){
-            res.status(400).json({ error: 'Missing parameters'});
-            return;
-        }
+  const newMedicine = {
+    name,
+    quantity,
+  }
 
-        const {db} = await connect();
+  const{ name, quantity } = req.body;
 
-        const medicineNameExists = await db
-            .collection('medicines')
-            .findOne({ name: name})
+  if (!name){
+    res.status(400).json({ error: 'Missing parameters'});
+    return;
+  }
 
+  if (req.method === "POST"){
 
-        const newMedicine = {
-            name,
-            quantity,
-        }
-
-        if(medicineNameExists){
-            await db.collection('medicines').updateOne({ name: name }, {$set: {status: newMedicine}});
-    
-            res.status(200).json(name);
-            return;
-        }
-        else{
-        const response = await db.collection('medicines').insertOne({
-            name, 
-            quantity
-        });
-        res.status(200).json(response.ops[0])}
+    if(!medicineNameExists){
+      const response = await db.collection('medicines').insertOne({
+        name,
+        quantity
+      });
+      res.status(200).json(response.ops[0])}
+      return;
     }
+
     else{
-        res.status(400).json({error: 'Wrong request Method'});
+      if (req.method === "PUT") {
+        if (medicineNameExists){
+          await db.collection('medicines').updateOne({ name: name }, {$set: {quantity: quantity}});
+          res.status(200).json(name);
+          return;
         }
-    }
+      }
 
+      res.status(400).json({error: 'Wrong request Method'});
+    }
+  }
